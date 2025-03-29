caaa326a5f370963071f07c71475aecf87fa5dda
sumnesh agrawal <sumnesha47@gmail.com>
2025-03-29 19:09:25 +0530

    fifth commit

---
+++ registry/new-york/hono-setup/api/[[...route]]/route.ts
@@ -0,0 +1,16 @@
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+
+const app = new Hono().basePath("/api");
+
+app.get("/hello", (c) => {
+  return c.json({
+    message: "Hello Next.js!",
+  });
+});
+
+export const GET = handle(app);
+export const POST = handle(app);
+export const PUT = handle(app);
+export const DELETE = handle(app);
+export const PATCH = handle(app);
